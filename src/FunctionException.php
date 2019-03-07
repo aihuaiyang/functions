8e5be1f760199b7b3abb36328798191b06338c2f
aihuaiyang <873811021@qq.com>
2019-03-07 14:48:22 +0800

    参数校验，增加异常断点

---
+++ src/FunctionException.php
@@ -0,0 +1,15 @@
+<?php
+namespace Huaiyang\Functions;
+use Exception;
+use Illuminate\Http\Response;
+
+class FunctionException extends Exception{
+    public function __construct(string $message = "", int $code = 500)
+    {
+        parent::__construct($message, $code);
+    }
+    public function render()
+    {
+        return response()->json(['result' => 0,'message' => $this->message], $this->code);
+    }
+}
